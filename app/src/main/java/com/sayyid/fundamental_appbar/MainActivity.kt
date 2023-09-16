8ccbf3b55e533bd8919f98d750b33a61a0b0eb02
sayyid <sayyidyaasiinalmahdi2@gmail.com>
2023-09-16 08:39:58 +0700

    update bab appbar selesai

--- app/src/main/java/com/sayyid/fundamental_appbar/MainActivity.kt
+++ app/src/main/java/com/sayyid/fundamental_appbar/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.sayyid.fundamental_appbar
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sayyid.fundamental_appbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setOnMenuItemClickListener{menuItem ->
+            when(menuItem.itemId){
+                R.id.menu1->{
+                    supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmnet_container,MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+                }
+            R.id.menu2 -> {
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> false
+            }
+        }
     }
 }
+
